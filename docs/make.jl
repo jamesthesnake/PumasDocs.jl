8e1773af85bb359a330650a376f9ebeac136d934
Andreas Noack <andreas@noack.dk>
2019-05-08 18:50:32 +0200

    Update Documenter version, comment out PuMaS, and adjust make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,39 +1,38 @@
-using Documenter,PuMaS
+using Documenter#=, PuMaS=#
 
-makedocs(modules=[PuMaS],
-         doctest=false, clean=true,
-         format =Documenter.HTML(),
-         sitename="PuMaS",
-         authors="Chris Rackauckas, Yingbo Ma, Joga Gobburu, Vijay Ivaturi",
-         pages = Any[
-         "Home" => "index.md",
-         "Tutorials" => Any[
-           "tutorials/introduction.md",
-         ],
-         "Basics" => Any[
-           "basics/overview.md",
-           "basics/models.md",
-           "basics/doses_subjects_populations.md",
-           "basics/simulation.md",
-           "basics/estimation.md",
-           "basics/nca.md",
-           "basics/faq.md",
-         ],
-         "Model Components" => Any[
-           "model_components/domains.md",
-           "model_components/dosing_control.md",
-           "model_components/dynamical_types.md",
-         ],
-         "Diagnostics" => Any[
-           "analysis/diagnostics.md",
-           "analysis/sensitivity.md",
-         ],
-         ])
+makedocs(
+  modules=Module[#=PuMaS=#],
+  doctest=false,
+  clean=true,
+  format =Documenter.HTML(),
+  sitename="PuMaS",
+  authors="Chris Rackauckas, Yingbo Ma, Joga Gobburu, Vijay Ivaturi",
+  pages = Any[
+    "Home" => "index.md",
+    "Tutorials" => Any[
+      "tutorials/introduction.md",
+    ],
+    "Basics" => Any[
+      "basics/overview.md",
+      "basics/models.md",
+      "basics/doses_subjects_populations.md",
+      "basics/simulation.md",
+      "basics/estimation.md",
+      "basics/nca.md",
+      "basics/faq.md",
+    ],
+    "Model Components" => Any[
+      "model_components/domains.md",
+      "model_components/dosing_control.md",
+      "model_components/dynamical_types.md",
+    ],
+    "Diagnostics" => Any[
+      "analysis/diagnostics.md",
+      "analysis/sensitivity.md",
+    ],
+  ]
+)
 
 deploydocs(
    repo = "github.com/UMCTM/PuMaSDocs.jl.git",
-   target = "build",
-   osname = "linux",
-   julia = "1.1",
-   deps = nothing,
-   make = nothing)
+)
